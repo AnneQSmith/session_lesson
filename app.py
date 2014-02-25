bc8446e6a3f9e4cf1735b336487c0293011eb094
Hackbright Student <info@hackbrightacademy.com>
2014-02-25 15:39:37 -0800

    working through flashing

--- app.py
+++ app.py
@@ -10,7 +10,19 @@ def index():
 
 @app.route("/", methods=["POST"])
 def process_login():
-    return render_template("login.html")
+    username = request.form.get("username")
+    password = request.form.get("password")
+    print "Im working yay!", username, password
+
+    if model.authenticate(username, password):
+        flash("User authenticated")
+        print (" model authenticated valid password")
+    else:
+        flash("Invalid credentials")
+        print ("model said no go on password")
+
+    return redirect(url_for("index"))
+  #  return render_template("login.html")
 
 @app.route("/register")
 def register():
